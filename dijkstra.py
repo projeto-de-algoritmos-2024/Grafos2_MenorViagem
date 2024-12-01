58068ff4db2b6b52bcf9bae40fb6dc7e472b3cc2
Brunna Louise Martins <brunna.louisemr@gmail.com>
2024-12-01 14:24:50 -0300

    adiciona nomes das cidades e integra input com algoritmo de dijkstra
    Co-authored-by: pkbceira03 <cabeceira2003@gmail.com>

--- dijkstra.py
+++ dijkstra.py
@@ -12,24 +12,6 @@ class Grafo:
         # grafo = dicionário já criado
         self.grafo = grafo
 
-    def busca_largura(self, inicio, fim):
-        arvore_busca = []
-        visitados = []
-        fila = deque([inicio])
-        fila.append(inicio)
-        visitados.append(inicio)
-        while (fila):
-            vertice = fila.popleft()
-            for vizinho in self.grafo.get(vertice, []):
-                if vizinho == fim:
-                    arvore_busca.append((vertice, vizinho))
-                    visitados.append(vizinho)
-                    return arvore_busca
-                elif not (vizinho in visitados):
-                    arvore_busca.append((vertice, vizinho))
-                    visitados.append(vizinho)
-                    fila.append(vizinho)
-
     def dijkstra(self, inicio, fim):
         min_arvore = minHeap.min_heap()
         min_arvore.inserir((inicio,0, inicio))
@@ -91,28 +73,28 @@ class Grafo:
     #tiro todos os outros caminho que poderiam chagar nele
     #volto pro começo
 
-grafo_feio = {
-    'A': [['B', 1, 'A'], ['C', 2, 'A']],
-    'B': [['A', 1, 'B']],
-    'C': [['A', 2, 'C'], ['D', 3, 'C']],
-    'D': [['C', 3, 'D']]
-}
+# grafo_feio = {
+#     'A': [['B', 1, 'A'], ['C', 2, 'A']],
+#     'B': [['A', 1, 'B']],
+#     'C': [['A', 2, 'C'], ['D', 3, 'C']],
+#     'D': [['C', 3, 'D']]
+# }
 
-grafo_bonito ={
-    's': [['2', 9, 's'],['6',14,'s'],['7',15,'s']],
-    '2': [['3',23,'2']],
-    '6': [['3', 18, '6'], ['5', 30, '6'], ['7',5,'6']],
-    '7': [['5',20,'7'], ['t',44,'7']],
-    '3': [['5',2,'3'], ['t',19,'3']],
-    '5': [['4', 11,'5'], ['t',16,'5']],
-    '4': [['3', 6,'4'], ['t',6,'4']] 
-}
+# grafo_bonito ={
+#     's': [['2', 9, 's'],['6',14,'s'],['7',15,'s']],
+#     '2': [['3',23,'2']],
+#     '6': [['3', 18, '6'], ['5', 30, '6'], ['7',5,'6']],
+#     '7': [['5',20,'7'], ['t',44,'7']],
+#     '3': [['5',2,'3'], ['t',19,'3']],
+#     '5': [['4', 11,'5'], ['t',16,'5']],
+#     '4': [['3', 6,'4'], ['t',6,'4']] 
+# }
 
-grafo = Grafo(grafo_bonito)
+# grafo = Grafo(grafo_bonito)
 
-primeiro = '5'
-fim = 't'
+# primeiro = '5'
+# fim = 't'
 
-p = grafo.dijkstra(primeiro, fim)
+# p = grafo.dijkstra(primeiro, fim)
 
-print(p)
+# print(p)
--- gps.py
+++ gps.py
@@ -2,6 +2,26 @@ import tkinter as tk
 import coordenadasMapa
 import meuMapa
 import math
+import minhas_cidades
+import dijkstra
+
+c = dijkstra.Grafo(meuMapa.mapa)
+
+def manipula_entrada(lista_cidades):
+    
+    lista_cidades_grafo = []
+    # separa por virgula
+    cidades_destino_origem = [p.strip() for p in lista_cidades.split(",")]
+    print("Cidades digitadas:", cidades_destino_origem)
+
+    # transforma a lista de compras em numeros que o grafo entende como seus nos, faz a legenda 
+    for cidade in cidades_destino_origem:
+        for minha_cidade in minhas_cidades.cidades:
+            if minhas_cidades.cidades[minha_cidade] == cidade:
+                lista_cidades_grafo.append(minha_cidade)
+    
+    print(lista_cidades_grafo)
+    return lista_cidades_grafo
 
 def desenha_caminho():
     raio_no = 10
@@ -36,6 +56,10 @@ def desenha_caminho():
             canvas.create_text(meio_x, meio_y, text=str(peso), fill="black")
 
 def gerenciador():
+    a = entrada.get()
+    b = manipula_entrada(a)
+    x = dijkstra.Grafo.dijkstra(c, b[0], b[1])
+    print(x)
     desenha_caminho()
 
 rota = tk.Tk()
@@ -43,12 +67,12 @@ rota.title("Mapa da Cidade")
 
 produtos_aceitos = tk.Label(
     rota,
-    text="Bem-vindo ao melhor gps da cidade"
+    text="Bem-vindo ao melhor gps da região de Eryndor"
 )
 
 produtos_aceitos.pack(pady=10)
 
-titulo = tk.Label(rota, text="De onde vc esta saindo e pra onde vc deseja ir?")
+titulo = tk.Label(rota, text="De onde vc esta saindo e pra onde vc deseja ir? 1 - Astrivana, 2 - Lunária, 3 - Eldrinor, 4 - Zirvanis, 5 - Norvalia, 6 - Erivale, 7 - Calthera, 8 - Drakmond, 9 - Silvaris, \n10 - Fenlaria, 11 - Torvindal, 12 - Krymora, 13 - Arvendil, 14 - Orynthia, 15 - Solthar, 16 - Quarion, 17 - Halveron, 18 - Morvanya, 19 - Velindre, \n20 - Galadore, 21 - Zyntheris, 22 - Etharion, 23 - Braxmoor, 24 - Lythoria, 25 - Eldenvale, 26 - Nyxhaven, 27 - Peryndor, 28 - Faeloria, 29 - Vorkalis, \n30 - Tharnwick, 31 - Myranor, 32 - Candralis, 33 - Arkeshal, 34 - Durnholde, 35 - Lymeris, 36 - Valtaren, 37 - Feronir, 38 - Talsyndra, 39 - Arwintha, \n40 - Drelmor, 41 - Cyrithal, 42 - Veskaris, 43 - Xeloria, 44 - Quindrel, 45 - Zaldara, 46 - Renfrost, 47 - Thalmyra, 48 - Ylverton, 49 - Fenwicke, 50 - Astrellyn.\n Digite a cidade de origem e a cidade de destino, separadas por vírgula e sem espaços.")
 titulo.pack(pady=10)
 
 entrada = tk.Entry(rota, width=50)
